63302cbd7c3b40177f97594a3f62573247d978e0
Jin Sun Park <jinsunpark917@gmail.com>
2023-05-17 17:28:35 +0900

    Added clicking on name in list shows student info functionality

--- app/students/page.tsx
+++ app/students/page.tsx
@@ -2,6 +2,7 @@
 
 import { AiOutlinePlus } from 'react-icons/ai';
 import styles from './students.module.css';
+import { useState, useEffect } from 'react';
 
 interface Student {
     id: number,
@@ -9,37 +10,54 @@ interface Student {
     subject: String
 }
 
-export async function getStudents() {
-    const res = await fetch('http://localhost:3000/api/students');
-    return res.json();
-}
-
-function StudentInfo() {
+function StudentInfo({student} : {student : Student}) {
+    if (!student) {
+        return (
+            <div className={styles.studentInfo}>
+            </div>
+        )
+    }
     return (
         <div className={styles.studentInfo}>
-
+            <p><span className={styles.fieldName}>Name: </span>{student.student_name}</p>
+            <p><span className={styles.fieldName}>Subject: </span>{student.subject}</p>
         </div>
     )
 }
 
-export default async function Students() {
-    const students = await getStudents();
+export default function Students() {
+    const [index, setIndex] = useState(0);
+    const [students, setStudents] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:3000/api/students')
+        .then(response => response.json())
+        .then(data => setStudents(data)
+    );
+    }, []);
+
     console.log(students);
-    
+
+    function handleClick(index: number) {
+        setIndex(index);
+    }
+
+    let student: Student = students[index];
+
     return (
         <div className={styles.container}>
             <div className={`${styles.studentListSection}`}>
                 <ul className={"menu bg-base-100 " + `${styles.studentList}`}>
                     { 
-                        students.map(({id, student_name, subject}: Student) => {
-                            return <li key={id} className="w-full"><a>{student_name}</a></li>
+                        students.map(({id, student_name}: Student, i: number) => {
+                            return <li key={id} className="w-full" onClick={() => handleClick(i)}><a className={index === i ? "active" : ""}>{student_name}</a></li>
                         })
                     }
                 </ul>
                 <button className="btn btn-outline w-full">Add New Student <AiOutlinePlus size={20} /></button>
             </div>
             <div className={styles.studentInfoSection}>
-                <StudentInfo />
+                <StudentInfo student={student} />
             </div>
         </div>
     )
