0347fe52aee64143a341a6997c784555ec9a5c91
Jin Sun Park <jinsunpark917@gmail.com>
2023-05-19 17:01:07 +0900

    Added edit student functionality

--- app/api/students/[id]/route.js
+++ app/api/students/[id]/route.js
@@ -1,8 +1,16 @@
 import { getConnection } from '../../../../utils/db';
 
 export async function DELETE(request, context) {
-    const id = context.params.id;
-    const connection = await getConnection();
-    const [rows] = await connection.execute(`DELETE FROM students WHERE id = ${id}`);
-    return new Response(JSON.stringify(rows));
-  }
+  const id = context.params.id;
+  const connection = await getConnection();
+  const [rows] = await connection.execute(`DELETE FROM students WHERE id = ${id}`);
+  return new Response(JSON.stringify(rows));
+}
+
+export async function PUT(request, context) {
+  const id = context.params.id;
+  const res = await request.json();
+  const connection = await getConnection();
+  const [rows] = await connection.execute(`UPDATE students SET name = '${res.name}', subject = '${res.subject}' WHERE id = ${id}`);
+  return new Response(JSON.stringify(rows));
+}
--- app/students/page.tsx
+++ app/students/page.tsx
@@ -1,9 +1,9 @@
 "use client"
 
 import { AiOutlinePlus } from 'react-icons/ai';
-import { HiOutlineTrash } from 'react-icons/hi';
+import { HiOutlineTrash, HiOutlinePencil } from 'react-icons/hi';
 import styles from './students.module.css';
-import { useState, useEffect, FormEventHandler, useRef, Dispatch } from 'react';
+import { useState, useEffect, FormEventHandler } from 'react';
 import Modal from '../components/Modal';
 
 interface Student {
@@ -27,7 +27,7 @@ function AddStudent({ getStudents }: StudentProps) {
         setSubject("");
     }
 
-    const handleSubmitNewStudent: FormEventHandler<HTMLFormElement> = (e) => {
+    const handleAdd: FormEventHandler<HTMLFormElement> = (e) => {
         e.preventDefault();
 
         const newStudent = {
@@ -50,7 +50,7 @@ function AddStudent({ getStudents }: StudentProps) {
         <>
             <button className="btn btn-outline btn-primary w-full" onClick={() => setIsModalOpen(true)}>Add New Student <AiOutlinePlus size={20} /></button>
             <Modal isModalOpen={isModalOpen}>
-                <form onSubmit={handleSubmitNewStudent}>
+                <form onSubmit={handleAdd}>
                     <h3 className="font-bold text-lg">Add New Student</h3>
                     <div className="modal-action flex justify-center">
                         <input
@@ -106,29 +106,96 @@ function DeleteStudent({student, getStudents} : {student: Student, getStudents: 
     );
 }
 
+function EditStudent({student, getStudents} : {student: Student, getStudents: () => void}) {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [name, setName] = useState(student.name + "");
+    const [subject, setSubject] = useState(student.subject + "");
+
+    const handleEdit: FormEventHandler<HTMLFormElement> = (e) => {
+        e.preventDefault();
+        
+        const editStudent = {
+            name: name,
+            subject: subject
+        }
+        
+        closeModal();
+
+        fetch(`http://localhost:3000/api/students/${student.id}`, {
+            method: "PUT",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify(editStudent)
+        }).then(() => {
+            getStudents();
+        });
+    }
+
+    function openModal() {
+        setIsModalOpen(true);
+        setName(student.name + "");
+        setSubject(student.subject + "");
+    }
+
+    function closeModal() {
+        setIsModalOpen(false);
+        setName("");
+        setSubject("");
+    }
+
+    return (
+        <>
+            <button className="btn btn-outline btn-success mr-2.5" onClick={openModal}>Edit <HiOutlinePencil size={20}/></button>
+            <Modal isModalOpen={isModalOpen}>
+                <form onSubmit={handleEdit}>
+                    <h3 className="font-bold text-lg">Edit Student</h3>
+                    <div className="modal-action flex justify-center">
+                        <input
+                            value={name}
+                            onChange={e => {setName(e.target.value)}} 
+                            type="text" 
+                            placeholder="Name" 
+                            className="input input-bordered w-full" 
+                        />
+                    </div>
+                    <div className="modal-action">
+                        <input
+                            value={subject} 
+                            onChange={e => {setSubject(e.target.value)}}
+                            type="text" 
+                            placeholder="Subject" 
+                            className="input input-bordered w-full" />
+                    </div>
+                    <div className="modal-action">
+                        <button type="reset" className="btn btn-ghost" onClick={closeModal}>Cancel</button>
+                        <button type="submit" className="btn">Save Changes</button>
+                    </div>
+                </form>
+            </Modal>
+        </>
+    )
+}
+
 function StudentInfo({student, getStudents} : {student: Student, getStudents: () => void}) {
     if (!student) {
-        return (
-            <div className={styles.studentInfo}>
-            </div>
-        )
+        return (<></>)
     }
     return (
-        <div className={styles.studentInfoContainer}>
+        <>
             <div className={styles.studentInfo}>
                 <p><span className="font-bold">Name: </span>{student.name}</p>
                 <p><span className="font-bold">Subject: </span>{student.subject}</p>
             </div>
             <div className={styles.studentInfoActions}>
+                <EditStudent student={student} getStudents={getStudents} />
                 <DeleteStudent student={student} getStudents={getStudents} />
             </div>
-        </div>
+        </>
     )
 }
 
 
 export default function Students() {
-    const [index, setIndex] = useState(0);
+    const [index, setIndex] = useState(-1);
     const [students, setStudents] = useState([]);
 
     async function getStudents() {
@@ -162,7 +229,9 @@ export default function Students() {
                 <AddStudent getStudents={getStudents} />
             </div>
             <div className={styles.studentInfoSection}>
-                <StudentInfo student={student} getStudents={getStudents} />
+                <div className={styles.studentInfoContainer}>
+                    <StudentInfo student={student} getStudents={getStudents} />
+                </div>
             </div>
         </div>
     )
