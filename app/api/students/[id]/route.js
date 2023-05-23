ed9d35abfc69b6735a77c466319b2d2a7d67fa65
Jin Sun Park <jinsunpark917@gmail.com>
2023-05-24 00:17:45 +0900

    Changed finances page UI

--- app/api/students/[id]/route.js
+++ app/api/students/[id]/route.js
@@ -1,10 +1,12 @@
 import { getConnection } from '../../../../utils/db';
 
+// TODO: proper response
 export async function DELETE(request, context) {
   const id = context.params.id;
   const connection = await getConnection();
-  const [rows] = await connection.execute(`DELETE FROM students WHERE id = ${id}`);
-  return new Response(JSON.stringify(rows));
+  const [rows_students] = await connection.execute(`DELETE FROM students WHERE id = ${id}`);
+  const [rows_finances] = await connection.execute(`DELETE FROM finances WHERE student_id = ${id}`);
+  return new Response(JSON.stringify({}));
 }
 
 export async function PUT(request, context) {
--- app/finances/page.tsx
+++ app/finances/page.tsx
@@ -3,6 +3,7 @@
 import styles from './finances.module.css';
 import Modal from '../components/Modal';
 import { HiOutlineTrash, HiOutlinePencil } from 'react-icons/hi';
+import { AiOutlinePlus } from 'react-icons/ai';
 import { useState, useEffect, FormEventHandler } from 'react';
 
 type Student = {
@@ -47,8 +48,8 @@ function AddIncome({ getFinances }: AddIncomeProps) {
         getStudents();
     }, []);
 
-    function closeModal() {
-        setIsModalOpen(false);
+    function openModal() {
+        setIsModalOpen(true);
         setDate("");
         setStudentId(-1);
         setAmount(0);
@@ -75,12 +76,17 @@ function AddIncome({ getFinances }: AddIncomeProps) {
         }).then(() => {
             getFinances();
         });
-        closeModal();
+        setIsModalOpen(false);
     }
 
     return (
         <>
-            <button className="btn btn-success" onClick={() => setIsModalOpen(true)}>Add Income</button>
+            <div className="tooltip" data-tip="Add Income">
+                <div className="shadow-md rounded-full p-3 mx-2 hover:bg-gray-50 cursor-pointer" onClick={openModal}>
+                    <AiOutlinePlus size={24} className="text-green-500" />
+                </div>
+            </div>
+            {/* <button className="btn btn-success" onClick={openModal}>Add Income <AiOutlinePlus size={20}/></button> */}
             <Modal isModalOpen={isModalOpen}>
                 <form onSubmit={handleAdd} className="w-full">
                     <h3 className="font-bold text-lg">Add New Income</h3>
@@ -119,7 +125,7 @@ function AddIncome({ getFinances }: AddIncomeProps) {
                         </select>
                     </div>
                     <div className="modal-action">
-                        <button type="reset" className="btn btn-ghost" onClick={closeModal}>Cancel</button>
+                        <button type="button" className="btn btn-ghost" onClick={() => setIsModalOpen(false)}>Cancel</button>
                         <button type="submit" className="btn">Add</button>
                     </div>
                 </form>
@@ -143,8 +149,10 @@ function DeleteIncome({getFinances, incomeEntry}: ModifyIncomeProps) {
 
     return (
         <>
-            <div className="tooltip" data-tip="Delete">
-                <HiOutlineTrash cursor="pointer" size={24} className="text-orange-500" onClick={() => setIsModalOpen(true)}/>
+            <div className="tooltip" data-tip="Delete Income">
+                <div className="shadow rounded-full p-3 mx-2">
+                    <HiOutlineTrash cursor="pointer" size={24} className="text-red-500" onClick={() => setIsModalOpen(true)}/>
+                </div>
             </div>
             <Modal isModalOpen={isModalOpen}>
                 <h3 className="font-bold text-lg text-center">Are you sure you want to delete this?</h3>
@@ -215,8 +223,10 @@ function EditIncome({getFinances, incomeEntry}: ModifyIncomeProps) {
 
     return (
         <>
-            <div className="tooltip" data-tip="Edit">
-                <HiOutlinePencil cursor="pointer" size={24} className="text-blue-500" onClick={openModal}/>
+            <div className="tooltip" data-tip="Edit Income">
+                <div className="shadow rounded-full p-3 mx-2">
+                    <HiOutlinePencil cursor="pointer" size={24} className="text-blue-500" onClick={openModal}/>
+                </div>
             </div>
             <Modal isModalOpen={isModalOpen}>
                 <form onSubmit={handleAdd} className="w-full">
@@ -265,8 +275,34 @@ function EditIncome({getFinances, incomeEntry}: ModifyIncomeProps) {
     )
 }
 
+function addCommas(number: number) {
+    // Convert the number to a string with commas
+    const formattedNumber = number.toLocaleString();
+  
+    // Remove any existing commas before adding new ones
+    const numberWithCommas = formattedNumber.replace(/,/g, '');
+  
+    // Add commas every three digits using a regular expression
+    const numberWithCommasFormatted = numberWithCommas.replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+  
+    return numberWithCommasFormatted;
+}
+
 export default function Finances() {
     const [finances, setFinances] = useState([]);
+    const [students, setStudents] = useState([]);
+    const [value, setValue] = useState<IncomeEntry>({ id: 0,
+        date: "",
+        student_name: "",
+        student_id: 0,
+        amount: 0,
+        received: 0});
+
+    async function getStudents() {
+        fetch("/api/students")
+        .then(response => response.json())
+        .then(data => setStudents(data))
+    }
 
     async function getFinances() {
         fetch("/api/finances")
@@ -276,14 +312,62 @@ export default function Finances() {
 
     useEffect(() => {
         getFinances();
+        getStudents();
     }, []);
 
     console.log(finances);
 
+    function getUniqueDates(substringStart: number, substringEnd: number) {
+        const getDates = finances.map((income: IncomeEntry) => income.date.substring(substringStart, substringEnd));
+        const getUniqueDates = new Set(getDates);
+        return Array.from(getUniqueDates);
+    }
+
     return (
         <div className={styles.container}>
-            <div className={styles.financeActions}>
-                <AddIncome getFinances={getFinances} />
+            <div className={styles.topBar}>
+                <div className="stats shadow mr-5">
+                    <div className="stat">
+                        <div className="stat-title">Total</div>
+                        <div className="stat-value">{addCommas(finances.reduce((acc, income: IncomeEntry) => acc + income.amount, 0))} KRW</div>
+                        <div className="flex">
+                            <div className="stat-desc text-green-500 mr-8">{addCommas(finances.filter((income: IncomeEntry) => income.received === 1).reduce((acc, income: IncomeEntry) => acc + income.amount, 0))} KRW received</div>
+                            <div className="stat-desc text-red-500">{addCommas(finances.filter((income: IncomeEntry) => income.received === 0).reduce((acc, income: IncomeEntry) => acc + income.amount, 0))} KRW not received</div>
+                        </div>
+                    </div>
+                </div>
+                <div className={`${styles.filterActions} shadow`}>
+                    <select className="select select-sm select-bordered max-w-xs mr-5">
+                        <option selected>All Years</option>
+                        { 
+                            getUniqueDates(0, 4).map((year) => {
+                                return <option>{year}</option>
+                            })
+                        }
+                    </select>
+                    <select className="select select-sm select-bordered max-w-xs mr-5">
+                        <option selected>All Months</option>
+                        {    
+                            getUniqueDates(5, 7).map((month) => {
+                                return <option>{month}</option>
+                            })
+                        }
+                    </select>
+                    <select className="select select-sm select-bordered max-w-xs mr-5">
+                        <option selected>All Students</option>
+                        { 
+                            students.map((student: Student) => {
+                                return <option>{student.name}</option>
+                            })
+                        }
+                    </select>
+                    <button className="btn btn-sm">Filter</button>
+                </div>
+                <div className={styles.financeActionsContainer}>
+                    <AddIncome getFinances={getFinances} />
+                    <EditIncome getFinances={getFinances} incomeEntry={value} />
+                    <DeleteIncome getFinances={getFinances} incomeEntry={value}/>
+                </div>
             </div>
             <div className="overflow-x-auto">
                 <table className="table table-compact w-full">
@@ -293,7 +377,7 @@ export default function Finances() {
                             <th>Student</th> 
                             <th>Amount (KRW)</th> 
                             <th>Received</th>
-                            <th>Actions</th> 
+                            {/* <th>Actions</th>  */}
                         </tr>
                     </thead> 
                     <tbody>
@@ -303,12 +387,12 @@ export default function Finances() {
                                     <tr className="bg-red-200" key={index}>
                                         <td>{value.date.split("T")[0]}</td>
                                         <td>{value.student_name}</td>
-                                        <td>{value.amount}</td>
+                                        <td>{addCommas(value.amount)}</td>
                                         <td className={value.received === 0 ? "bg-red-200" : "bg-green-200"}>{value.received === 0 ? "No" : "Yes"}</td>
-                                        <td className="flex gap-5">
+                                        {/* <td className="flex gap-5">
                                             <EditIncome getFinances={getFinances} incomeEntry={value} />
                                             <DeleteIncome getFinances={getFinances} incomeEntry={value}/>
-                                        </td>
+                                        </td> */}
                                     </tr>
                                 )
                             })
