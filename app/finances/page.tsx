22cf03e7a2252ba061db926b357e301b4a60ffdf
Jin Sun Park <jinsunpark917@gmail.com>
2023-05-24 23:35:08 +0900

    Added filter income

--- app/finances/page.tsx
+++ app/finances/page.tsx
@@ -3,8 +3,7 @@
 import styles from './finances.module.css';
 import Modal from '../components/Modal';
 import { addCommas } from '../../utils/formatting';
-import { HiOutlineTrash, HiOutlinePencil } from 'react-icons/hi';
-import { AiOutlinePlus } from 'react-icons/ai';
+import { HiOutlinePencil } from 'react-icons/hi';
 import { ImCheckmark, ImCross } from 'react-icons/im';
 import { BsTrash3Fill } from 'react-icons/bs';
 import { useState, useEffect, FormEventHandler, ChangeEvent } from 'react';
@@ -98,7 +97,7 @@ function AddIncome({ getFinances }: AddIncomeProps) {
 
     return (
         <>
-            <button className="btn btn-sm btn-outline btn-info w-9/12" onClick={openModal}>Add Income</button>
+            <button className="btn btn-sm btn-outline btn-info" onClick={openModal}>Add Income</button>
             {/* <button className="btn btn-success" onClick={openModal}>Add Income <AiOutlinePlus size={20}/></button> */}
             <Modal isModalOpen={isModalOpen}>
                 <form onSubmit={handleAdd} className="w-full">
@@ -316,6 +315,10 @@ export default function Finances() {
     const [finances, setFinances] = useState<IncomeEntry[]>([]);
     const [students, setStudents] = useState([]);
     const [allRowsSelected, setAllRowsSelected] = useState(false);
+    const [year, setYear] = useState(0);
+    const [month, setMonth] = useState("0");
+    const [studentId, setStudentId] = useState(-1);
+    const [filteredFinances, setFilteredFinances] = useState<IncomeEntry[]>([]);
 
     async function getStudents() {
         fetch("/api/students")
@@ -330,9 +333,15 @@ export default function Finances() {
             const rows = data.map((value: IncomeEntry) => {
                 return {...value, isSelected: false}
             });
+            console.log("ROWS");
+            console.log(rows);
             setFinances(rows);
+            console.log("HERE");
+            console.log(filterEntries(year, month, studentId, rows));
+            setFilteredFinances(filterEntries(year, month, studentId, rows));
         })
     }
+
 
     useEffect(() => {
         getFinances();
@@ -341,7 +350,7 @@ export default function Finances() {
 
     console.log(finances);
 
-    const selectedRows = finances.filter((entry: IncomeEntry) => entry.isSelected);
+    const selectedRows = filteredFinances.filter((entry: IncomeEntry) => entry.isSelected);
     console.log(selectedRows);
 
     function getUniqueDates(substringStart: number, substringEnd: number) {
@@ -352,19 +361,53 @@ export default function Finances() {
 
     function handleIncomeSelectChange(e: ChangeEvent<HTMLInputElement>, index: number, value: IncomeEntry) {
         console.log(e.target.checked);
-        const newFinances: IncomeEntry[] = [...finances];
+        const newFinances: IncomeEntry[] = [...filteredFinances];
         newFinances[index] = {...value, isSelected: e.target.checked};
-        setFinances(newFinances);
+        setFilteredFinances(newFinances);
     }
 
     function selectAllRows(e: ChangeEvent<HTMLInputElement>) {
-        const newFinances = finances.map((income: IncomeEntry) => {
+        const newFinances = filteredFinances.map((income: IncomeEntry) => {
             return {
                 ...income, 
                 isSelected: e.target.checked
             }});
-        setFinances(newFinances);
+        setFilteredFinances(newFinances);
         setAllRowsSelected(e.target.checked);
+    }
+
+    function filterEntries(year: number, month: string, studentId: number, entries: IncomeEntry[]) {
+        let filteredEntries = [...entries];
+        console.log(filterEntries);
+        if (year != 0) {
+            console.log("Enter year");
+            filteredEntries = filteredEntries.filter((entry) => parseInt(entry.date.substring(0, 4)) === year);
+        }
+
+        if (month !== "0") {
+            console.log("Enter month");
+            console.log(month);
+            filteredEntries = filteredEntries.filter((entry) => entry.date.substring(5, 7) === month);
+        }
+
+        if (studentId != -1) {
+            console.log("Enter student");
+            console.log(studentId);
+            filteredEntries = filteredEntries.filter((entry) => entry.student_id === studentId);
+        }
+        console.log(filteredEntries);
+        return filteredEntries;
+    }
+
+    function handleFilterEntries() {
+        setFilteredFinances(filterEntries(year, month, studentId, finances));
+    }
+
+    function showAllEntries() {
+        setFilteredFinances(finances);
+        setYear(0);
+        setMonth("0");
+        setStudentId(-1);
     }
 
     return (
@@ -373,15 +416,15 @@ export default function Finances() {
                 <div className="stats shadow mr-5 mb-5">
                     <div className="stat">
                         <div className="stat-title">Total</div>
-                        <div className="stat-value mt-2">{addCommas(finances.reduce((acc, income: IncomeEntry) => acc + income.amount, 0))} KRW</div>
+                        <div className="stat-value mt-2">{addCommas(filteredFinances.reduce((acc, income: IncomeEntry) => acc + income.amount, 0))} KRW</div>
                         <div className="mt-3">
-                            <div className="stat-desc text-green-500 mr-8">{addCommas(finances.filter((income: IncomeEntry) => income.received === 1).reduce((acc, income: IncomeEntry) => acc + income.amount, 0))} KRW received</div>
-                            <div className="stat-desc text-red-500">{addCommas(finances.filter((income: IncomeEntry) => income.received === 0).reduce((acc, income: IncomeEntry) => acc + income.amount, 0))} KRW not received</div>
+                            <div className="stat-desc text-green-500 mr-8">{addCommas(filteredFinances.filter((income: IncomeEntry) => income.received === 1).reduce((acc, income: IncomeEntry) => acc + income.amount, 0))} KRW received</div>
+                            <div className="stat-desc text-red-500">{addCommas(filteredFinances.filter((income: IncomeEntry) => income.received === 0).reduce((acc, income: IncomeEntry) => acc + income.amount, 0))} KRW not received</div>
                         </div>
                     </div>
                 </div>
                 <div className={`${styles.filterActions} shadow`}>
-                    <select className="select select-sm select-bordered max-w-xs mr-5" defaultValue={0}>
+                    <select className="select select-sm select-bordered max-w-xs mr-5" value={year} onChange={(e) => setYear(parseInt(e.target.value))}>
                         <option value={0}>All Years</option>
                         { 
                             getUniqueDates(0, 4).map((year) => {
@@ -389,23 +432,24 @@ export default function Finances() {
                             })
                         }
                     </select>
-                    <select className="select select-sm select-bordered max-w-xs mr-5" defaultValue={0}>
-                        <option value={0}>All Months</option>
+                    <select className="select select-sm select-bordered max-w-xs mr-5" value={month} onChange={(e) => setMonth(e.target.value)}>
+                        <option value={"0"}>All Months</option>
                         {    
                             getUniqueDates(5, 7).map((month) => {
                                 return <option key={month} value={month}>{month}</option>
                             })
                         }
                     </select>
-                    <select className="select select-sm select-bordered max-w-xs mr-5" defaultValue={0}>
-                        <option value={0}>All Students</option>
+                    <select className="select select-sm select-bordered max-w-xs mr-5" value={studentId} onChange={(e) => setStudentId(parseInt(e.target.value))}>
+                        <option value={-1}>All Students</option>
                         { 
                             students.map((student: Student) => {
                                 return <option key={student.id} value={student.id}>{student.name}</option>
                             })
                         }
                     </select>
-                    <button className="btn btn-sm">Filter</button>
+                    <button className="btn btn-sm" onClick={handleFilterEntries}>Filter</button>
+                    <button className="btn btn-sm ml-2" onClick={showAllEntries}>Show All</button>
                 </div>
                 <div className={styles.financeActionsOuterContainer}>
                     <AddIncome getFinances={getFinances} />
@@ -434,7 +478,7 @@ export default function Finances() {
                     </thead> 
                     <tbody>
                         {
-                            finances.map((value: IncomeEntry, index) => {
+                            filteredFinances.map((value: IncomeEntry, index) => {
                                 return (
                                     <tr className="bg-red-200" key={index}>
                                         <td>
